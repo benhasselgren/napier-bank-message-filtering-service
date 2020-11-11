448e8193f2d13b0e14465dd87feeba7937d6eb6e
Ben Hasselgren <ben.nadalito@gmail.com>
2020-11-11 13:42:32 +0000

    added data facade class

---
+++ NapierBankMessageFilteringService/DataLayer/DataFacade.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Classes
+{
+    /// <summary>
+    /// Class <c>DataFacade</c> 
+    /// A facade that the business lay can communicate with to create, load and save messages
+    /// </summary>
+    class DataFacade
+    {
+        // ------------------ Instance Variables ------------------
+        private FileSingleton file;
+        private MessageFactory messageFactory;
+        private List<Message> unprocessedMessages;
+
+        // ------------------ Constructors ------------------
+
+        // ------------------ Getters/Setters ------------------
+        internal FileSingleton File { get => file; set => file = value; }
+        internal MessageFactory MessageFactory { get => messageFactory; set => messageFactory = value; }
+        internal List<Message> UnprocessedMessages { get => unprocessedMessages; set => unprocessedMessages = value; }
+
+        // ------------------ Methods ------------------
+        /// <summary>
+        /// Method <c>loadMessages</c> 
+        /// Loads messages from a file
+        /// </summary>
+        private void loadMessages(string file)
+        {
+
+        }
+
+        /// <summary>
+        /// Method <c>createMessage</c> 
+        /// Creates a message from user input
+        /// </summary>
+        private void createMessage(string body, string header)
+        {
+
+        }
+
+        /// <summary>
+        /// Method <c>saveMessage</c> 
+        /// Saves processed messaged to a json file
+        /// </summary>
+        private void saveMessage(List<Message> processedMessages)
+        {
+
+        }
+    }
+}
