df332fbbf921f104d7a93b179c161c6232ed455e
Ben Hasselgren <ben.nadalito@gmail.com>
2020-11-11 14:26:04 +0000

    added hashtag class

---
+++ NapierBankMessageFilteringService/DataLayer/Classes/Hashtag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Classes
+{
+    /// <summary>
+    /// Class <c>Hashtag</c> 
+    /// A class that outlines a hashtag
+    /// </summary>
+    class Hashtag
+    {
+        // ------------------ Instance Variables ------------------
+        private string title;
+        private int count;
+
+        // ------------------ Constructors ------------------
+        /// <summary>
+        /// Constructor <c>title</c> 
+        /// </summary>
+        public Hashtag(string title)
+        {
+            this.title = title;
+            this.count = 1;
+        }
+
+        // ------------------ Getters/Setters ------------------
+        public string Title { get => title; set => title = value; }
+        public int Count { get => count; set => count = value; }
+
+        // ------------------ Methods ------------------
+        /// <summary>
+        /// Method <c>incrementCount</c> 
+        /// Increases count by 1
+        /// </summary>
+        public void incrementCount()
+        {
+            this.Count++;
+        }
+    }
+}
